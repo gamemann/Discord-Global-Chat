207319cd96b2f8e3196406e7bd5de6dd313fe092
gamemann <gamemann@gflclan.com>
2021-04-01 01:07:50 +0000

    Organize code.

--- src/setup.py
+++ src/setup.py
@@ -4,9 +4,8 @@ import sqlite3
 
 def setuptables(conn):
     cur = conn.cursor()
-
+    
     cur.execute("CREATE TABLE IF NOT EXISTS `channels` (id integer PRIMARY KEY AUTOINCREMENT, guildid integer, channelid integer)")
-
     conn.commit()
 
     print("[SETUP] Created all tables.")
