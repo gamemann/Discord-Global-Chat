2c5a005fe6bf7c7240a0d46c7a092773738b399e
gamemann <gamemann@gflclan.com>
2021-04-01 01:07:32 +0000

    Add setup Python file.

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+import fileinput
+import sys
+import sqlite3
+
+def setuptables(conn):
+    cur = conn.cursor()
+
+    cur.execute("CREATE TABLE IF NOT EXISTS `channels` (id integer PRIMARY KEY AUTOINCREMENT, guildid integer, channelid integer)")
+
+    conn.commit()
+
+    print("[SETUP] Created all tables.")
