a6f85d515438538ed09801d5093567e226929fde
gamemann <gamemann@gflclan.com>
2021-04-01 03:03:07 +0000

    Restructuring SQL table.

--- src/setup.py
+++ src/setup.py
@@ -4,8 +4,8 @@ import sqlite3
 
 def setuptables(conn):
     cur = conn.cursor()
-    
-    cur.execute("CREATE TABLE IF NOT EXISTS `channels` (id integer PRIMARY KEY AUTOINCREMENT, guildid integer, channelid integer)")
+
+    cur.execute("CREATE TABLE IF NOT EXISTS `channels` (guildid integer PRIMARY KEY, channels string, webhookurl string)")
     conn.commit()
 
     print("[SETUP] Created all tables.")
