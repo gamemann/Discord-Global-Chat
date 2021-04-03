a673e8e0b26e807882c927cef7c0363127bac308
gamemann <gamemann@gflclan.com>
2021-04-03 21:44:41 +0000

    Add defaults.

--- src/config.py
+++ src/config.py
@@ -2,6 +2,11 @@ import json
 
 def getconfig(cfgfile):
     cfg = {}
+
+    # Set defaults.
+    cfg['BotMsgStayTime'] = 10.0
+    cfg['UpdateTime'] = 60.0
+    cfg['AppendGuildName'] = True
     
     with open(cfgfile) as f:
         cfg = json.load(f)
